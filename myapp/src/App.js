d101db5536eca8ac9f084098c6709b90607b04fa
mahidurgam <105271766+mahidurgam@users.noreply.github.com>
2022-12-09 14:47:07 +0530

    added 4th line Push

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -7,6 +7,7 @@ function App() {
      <h1>Practicing Clone Push Pull from my local repository</h1>
 <h1>Welcome to github</h1>
 <h1>Clone :git clone is primarily used to point to an existing repo and make a clone or copy of that repo at in a new directory, at another location.</h1>
+    <h1>Push: git push updates the remote branch with local commits </h1>
     </div>
   );
 }
