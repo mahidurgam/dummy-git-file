36c0e8faf361ad4ac17ede646340b77f61a034ea
Mahi Durgam <uuma8628@gmail.com>
2022-12-09 14:48:29 +0530

    added 5th line of Pull

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -8,6 +8,9 @@ function App() {
 <h1>Welcome to github</h1>
 <h1>Clone :git clone is primarily used to point to an existing repo and make a clone or copy of that repo at in a new directory, at another location.</h1>
     <h1>Push: git push updates the remote branch with local commits </h1>
+    <h1>
+      Pull :Pull requests let you tell others about changes you've pushed to a branch in a repository on GitHub
+    </h1>
     </div>
   );
 }
